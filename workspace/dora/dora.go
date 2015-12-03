934e0b9b19389088dab303ac685e83f56d9bd204
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-12-03 17:10:39 -0500

    dora: restructure main loop and pull out functions

--- workspace/dora/dora.go
+++ workspace/dora/dora.go
@@ -12,24 +12,6 @@ func check(e error) {
 		panic(e)
 	}
 }
-
-// dora the explora
-type Dora struct {
-	ws *w.Workspace
-	ac ArtifactCollection
-}
-
-func New(ws *w.Workspace) (*Dora, error) {
-	// TODO: get this from a real place
-	ac, e := NewLoggingArtifactCollection()
-	check(e)
-
-	return &Dora{
-		ws: ws,
-		ac: ac,
-	}, nil
-}
-
 func isBBEnd(insn gapstone.Instruction) bool {
 	return w.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_CALL) ||
 		w.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_JUMP) ||
@@ -49,12 +31,6 @@ func GetNextInstructionPointer(emu *w.Emulator, sman *w.SnapshotManager) (w.VA, 
 	})
 	return va, e
 }
-
-type todoPath struct {
-	state w.SnapshotManagerCookie
-	va    w.VA
-}
-
 func IsConditionalJump(insn gapstone.Instruction) bool {
 	if w.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_JUMP) && insn.Mnemonic != "jmp" {
 		return true
@@ -97,6 +73,122 @@ func GetJumpTargets(emu *w.Emulator, insn gapstone.Instruction) ([]w.VA, error) 
 	return ret, nil
 }
 
+// dora the explora
+type Dora struct {
+	ws *w.Workspace
+	ac ArtifactCollection
+}
+
+func New(ws *w.Workspace) (*Dora, error) {
+	// TODO: get this from a real place
+	ac, e := NewLoggingArtifactCollection()
+	check(e)
+
+	return &Dora{
+		ws: ws,
+		ac: ac,
+	}, nil
+}
+
+type todoPath struct {
+	state w.SnapshotManagerCookie
+	va    w.VA
+}
+
+type FunctionExplorer struct {
+	emu     *w.Emulator
+	sman    *w.SnapshotManager
+	todo    []todoPath
+	hits    map[w.VA]bool
+	startSp w.VA
+	ac      ArtifactCollection
+}
+
+func (s *FunctionExplorer) pushState(va w.VA) error {
+	here, e := s.sman.Push()
+	check(e)
+	s.todo = append(s.todo, todoPath{state: here, va: va})
+	return nil
+}
+
+func (s *FunctionExplorer) popState() error {
+	if len(s.todo) == 0 {
+		// TODO: handle a real error
+		panic("no paths to explore")
+	}
+	path := s.todo[len(s.todo)-1]
+	s.todo = s.todo[1:]
+	log.Printf("exploring path %s: va=0x%x", path.state, path.va)
+	check(s.sman.RevertUntil(path.state))
+	s.emu.SetInstructionPointer(path.va)
+	return nil
+}
+
+func (s *FunctionExplorer) hasExploredThisAddressBefore() bool {
+	_, ok := s.hits[s.emu.GetInstructionPointer()]
+	return ok
+}
+
+func (s *FunctionExplorer) markThisAddressExplored() error {
+	s.hits[s.emu.GetInstructionPointer()] = true
+	return nil
+}
+
+func (s *FunctionExplorer) tracePc() error {
+	str, _, e := s.emu.FormatAddress(s.emu.GetInstructionPointer())
+	check(e)
+	color.Set(color.FgHiBlack)
+	log.Printf("ip:" + str)
+	color.Unset()
+	return nil
+}
+
+func (s *FunctionExplorer) handleRet(insn gapstone.Instruction) error {
+	log.Printf("returning, done.")
+	afterSp := s.emu.GetStackPointer()
+	stackDelta := uint64(afterSp) - uint64(s.startSp)
+	log.Printf("stack delta: 0x%x", stackDelta)
+	return nil
+}
+
+func (s *FunctionExplorer) handleConditionalJump(insn gapstone.Instruction) error {
+	targets, e := GetJumpTargets(s.emu, insn)
+	check(e)
+	if len(targets) < 2 {
+		// TODO: by definition, a conditional jump should have at least two cases...
+		panic("len(targets) < 2")
+	}
+
+	nextPc := targets[0]
+	for _, target := range targets[1:] {
+		log.Printf("other target: 0x%x", target)
+		check(s.pushState(target))
+	}
+	s.emu.SetInstructionPointer(nextPc)
+	return nil
+}
+
+func (s *FunctionExplorer) handleCall(insn gapstone.Instruction) error {
+	// TODO: try to resolve imports before blindly emulating
+	callPc, e := GetNextInstructionPointer(s.emu, s.sman)
+	if e == nil {
+		log.Printf("  call target: 0x%x", callPc)
+	}
+	s.ac.AddCallXref(CallCrossReference{s.emu.GetInstructionPointer(), callPc})
+
+	nextPc := w.VA(uint64(s.emu.GetInstructionPointer()) + uint64(insn.Size))
+	s.emu.SetInstructionPointer(nextPc)
+
+	// TODO: need to detect calling convention, and in the case of stdcall,
+	//   cleanup the stack
+
+	return nil
+}
+
+func (s *FunctionExplorer) handleGeneralInstruction(insn gapstone.Instruction) error {
+	return s.emu.StepOver()
+}
+
 // things yet to discover:
 //   OK: final stack delta
 //   TODO: arguments passed in registers
@@ -117,11 +209,18 @@ func (dora *Dora) ExploreFunction(va w.VA) error {
 	check(e)
 	defer sman.Close()
 
+	ex := &FunctionExplorer{
+		emu:     emu,
+		sman:    sman,
+		todo:    []todoPath{},
+		hits:    make(map[w.VA]bool),
+		startSp: emu.GetStackPointer(),
+		ac:      dora.ac,
+	}
+
 	bbStart := va
 	emu.SetInstructionPointer(va)
 	check(e)
-
-	beforeSp := emu.GetStackPointer()
 
 	// TODO: how to disable these while on an excursion?
 	rh, e := emu.HookMemRead(func(access int, addr w.VA, size int, value int64) {
@@ -136,99 +235,49 @@ func (dora *Dora) ExploreFunction(va w.VA) error {
 	check(e)
 	defer wh.Close()
 
-	hitVas := make(map[w.VA]bool)
+	check(ex.pushState(va))
 
-	var todoPaths = []todoPath{}
-	here, e := sman.GetCurrentCookie()
-	check(e)
-	todoPaths = append(todoPaths, todoPath{state: here, va: va})
-
-	for len(todoPaths) > 0 {
-		path := todoPaths[len(todoPaths)-1]
-		todoPaths = todoPaths[1:]
-		log.Printf("exploring path %s: va=0x%x", path.state, path.va)
-		check(sman.RevertUntil(path.state))
-		emu.SetInstructionPointer(path.va)
-
+	// TODO: don't reach
+	for len(ex.todo) > 0 {
+		check(ex.popState())
 		for {
-			// TODO: dora.checkVisitedVas()
-			if _, ok := hitVas[emu.GetInstructionPointer()]; ok {
+			if ex.hasExploredThisAddressBefore() {
+				log.Print("bad va")
 				break
 			}
 
-			// TODO: dora.tracePc()
-			s, _, e := emu.FormatAddress(emu.GetInstructionPointer())
-			check(e)
-			color.Set(color.FgHiBlack)
-			log.Printf("ip:" + s)
-			color.Unset()
+			ex.tracePc()
 
-			insn, e := emu.GetCurrentInstruction()
+			insn, e := ex.emu.GetCurrentInstruction()
 			check(e)
+
+			check(ex.markThisAddressExplored())
 
 			if isBBEnd(insn) {
 				e := dora.ac.AddBasicBlock(BasicBlock{Start: bbStart, End: emu.GetInstructionPointer()})
 				check(e)
 			}
 
-			beforePc := emu.GetInstructionPointer()
-			// TODO: dora.handleRet()
-			// TODO: dora.handleConditionalJump()
-			// TODO: dora.handleJump()
-			// TODO: dora.handleCall()
-			// TODO: dora.handleStep()
+			beforePc := ex.emu.GetInstructionPointer()
 			if w.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_RET) ||
 				w.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_IRET) {
-				log.Printf("returning, done.")
-				afterSp := emu.GetStackPointer()
-				stackDelta := uint64(afterSp) - uint64(beforeSp)
-				log.Printf("stack delta: 0x%x", stackDelta)
+				check(ex.handleRet(insn))
 				break
 			} else if IsConditionalJump(insn) {
-				targets, e := GetJumpTargets(emu, insn)
-				check(e)
-				if len(targets) < 2 {
-					// TODO: by definition, a conditional jump should have at least two cases...
-					panic("len(targets) < 2")
-				}
-
-				here, e := sman.Push()
-				check(e)
-
-				nextPc := targets[0]
-				for _, target := range targets[1:] {
-					log.Printf("other target: 0x%x", target)
-					todoPaths = append(todoPaths, todoPath{state: here, va: target})
-				}
-				emu.SetInstructionPointer(nextPc)
-
+				check(ex.handleConditionalJump(insn))
 			} else if w.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_CALL) {
-				// TODO: try to resolve imports before blindly emulating
-				callPc, e := GetNextInstructionPointer(emu, sman)
-				if e == nil {
-					log.Printf("  call target: 0x%x", callPc)
-				}
-				dora.ac.AddCallXref(CallCrossReference{emu.GetInstructionPointer(), callPc})
-
-				nextPc := w.VA(uint64(emu.GetInstructionPointer()) + uint64(insn.Size))
-				emu.SetInstructionPointer(nextPc)
-
-				// TODO: need to detect calling convention, and in the case of stdcall,
-				//   cleanup the stack
-
+				check(ex.handleCall(insn))
 			} else {
-				e = emu.StepOver()
+				e := ex.handleGeneralInstruction(insn)
 				if e != nil {
 					log.Printf("error: %s", e.Error())
 					break
 				}
+				log.Print("ok")
 			}
 
 			// TODO: fetch stack read/write set for arg/variable detection
 			// TODO: fetch reg read/write set for arg detection
-
-			// TODO: dora.updateVisitedVas()
-			hitVas[beforePc] = true
 
 			afterPc := emu.GetInstructionPointer()
 			if isBBEnd(insn) {
