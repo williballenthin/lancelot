ab1f2324049abf7dcaa769c669468da930843ea2
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-12-28 18:50:56 -0500

    linear disassembly: add doc

--- workspace/dora/linear_disassembly/linear_disassembly.go
+++ workspace/dora/linear_disassembly/linear_disassembly.go
@@ -1,3 +1,6 @@
+// LinearDisassembly implements a code explorer that uses linear
+//  disassembly to recognize instructions, basic blocks, and control
+//  flow edges.
 package LinearDisassembly
 
 import (
@@ -14,20 +17,39 @@ func check(e error) {
 	}
 }
 
+// JumpType defines the possible types of intra-function edges.
 type JumpType string
 
+// JumpTypeCondTrue is the JumpType that represents the True
+//  edge of a conditional branch.
 var JumpTypeCondTrue JumpType = "jtrue"
+
+// JumpTypeCondFalse is the JumpType that represents the False
+//  edge of a conditional branch.
 var JumpTypeCondFalse JumpType = "jfalse"
+
+// JumpTypeUncond is the JumpType that represents the edge of
+//  an unconditional branch.
 var JumpTypeUncond JumpType = "juncond"
 
+// JumpTarget describes the destination of an edge.
 type JumpTarget struct {
 	Va       w.VA
 	JumpType JumpType
 }
 
-type InstructionTraceHandler func(va w.VA, insn gapstone.Instruction) error
-type JumpTraceHandler func(va w.VA, insn gapstone.Instruction, jump JumpTarget) error
+// InstructionTraceHandler is a function that can process instructions
+//  parsed by this package.
+// Use insn.Address for the current address.
+type InstructionTraceHandler func(insn gapstone.Instruction) error
 
+// JumpTraceHandler is a function that can process control flow edges
+//  parsed by this package.
+// Use insn.Address for the source address.
+// Use bb for the address of the source basic block.
+type JumpTraceHandler func(insn gapstone.Instruction, bb w.VA, jump JumpTarget) error
+
+// LD is the object that holds the state of a linear disassembler.
 type LD struct {
 	disassembler gapstone.Engine
 	insnHandlers []InstructionTraceHandler
@@ -62,7 +84,9 @@ func (ld *LD) RegisterJumpTraceHandler(fn JumpTraceHandler) error {
 	return nil
 }
 
-// move to utils
+// ReadInstruction fetches bytes from the provided address space at the given
+//  address and parses them into a single instruction instance.
+// TODO: move to utils
 func ReadInstruction(dis gapstone.Engine, as w.AddressSpace, va w.VA) (gapstone.Instruction, error) {
 	d, e := as.MemRead(va, uint64(w.MAX_INSN_SIZE))
 	check(e)
@@ -93,8 +117,13 @@ func min(a uint64, b uint64) uint64 {
 	}
 }
 
-// move to utils
-// return: data at va formatted appropriately, number of bytes for va formatted, error
+// FormatAddressDisassembly formats the bytes at a given address in a given
+//  address space as disassembly.
+// It may also include the hexidecimal bytes alongside the mnemonics and
+//  operands if numOpcodeBytes is non-zero.
+// This function returns the data at va formatted appropriately, the number
+//  of bytes for va formatted, and an error instance.
+// TODO: move to utils
 func FormatAddressDisassembly(dis gapstone.Engine, as w.AddressSpace, va w.VA, numOpcodeBytes uint) (string, uint64, error) {
 	insn, e := ReadInstruction(dis, as, va)
 	check(e)
@@ -104,7 +133,7 @@ func FormatAddressDisassembly(dis gapstone.Engine, as w.AddressSpace, va w.VA, n
 	check(e)
 
 	// format each of those as hex
-	bytesPrefix := make([]string, 0)
+	var bytesPrefix []string
 	for _, b := range d {
 		bytesPrefix = append(bytesPrefix, fmt.Sprintf("%02X", b))
 	}
@@ -120,6 +149,8 @@ func FormatAddressDisassembly(dis gapstone.Engine, as w.AddressSpace, va w.VA, n
 
 // GetJumpTarget gets the address to which a known jump instruction
 //  transfers control.
+// If the instruction is a conditional jump, then this function returns
+//  the "jump is taken" target.
 func GetJumpTarget(insn gapstone.Instruction) (w.VA, error) {
 	// have the following possibilities:
 	//   - direct jump: jmp 0x1000
@@ -238,13 +269,21 @@ func (ld *LD) ExploreBB(as w.AddressSpace, va w.VA) ([]w.VA, error) {
 	return nextBBs, nil
 }
 
+// ExploreFunction linearly disassembles instructions and explores basic
+//  blocks starting at a given address in a given address space, invoking
+//  appropriate callbacks.
+// It terminates once it has explored all the basic blocks it discovers.
 func (ld *LD) ExploreFunction(as w.AddressSpace, va w.VA) error {
+	// lifo is a stack (cause these are easier than queues in Go) of BBs
+	//  that need to be explored.
 	lifo := make([]w.VA, 0, 10)
 	lifo = append(lifo, va)
 
+	// the set of explored BBs, by BB start address
 	doneBBs := map[w.VA]bool{}
 
 	for len(lifo) > 0 {
+		// pop BB address
 		bb := lifo[len(lifo)-1]
 		lifo = lifo[:len(lifo)-1]
 
@@ -259,6 +298,7 @@ func (ld *LD) ExploreFunction(as w.AddressSpace, va w.VA) error {
 			return e
 		}
 
+		// push new BB addresses
 		lifo = append(lifo, next...)
 	}
 
