8fcf365410f473e3e5767bb5369ccb0fe731f08a
Willi Ballenthin <willi.ballenthin@gmail.com>
2016-01-08 21:09:36 -0500

    run ld: clean up imports

--- app/linear_disassembler/run_ld.go
+++ app/linear_disassembler/run_ld.go
@@ -5,14 +5,12 @@ import (
 	"fmt"
 	"github.com/bnagy/gapstone"
 	"github.com/codegangsta/cli"
-	AS "github.com/williballenthin/Lancelot/address_space"
-	EP "github.com/williballenthin/Lancelot/analysis/file/entry_point"
-	Pr "github.com/williballenthin/Lancelot/analysis/file/prologue"
-	DCA "github.com/williballenthin/Lancelot/analysis/function/direct_calls"
-	N "github.com/williballenthin/Lancelot/analysis/function/name"
-	SDA "github.com/williballenthin/Lancelot/analysis/function/stack_delta"
+	entry_point_analysis "github.com/williballenthin/Lancelot/analysis/file/entry_point"
+	prologue_analysis "github.com/williballenthin/Lancelot/analysis/file/prologue"
+	direct_call_analysis "github.com/williballenthin/Lancelot/analysis/function/direct_calls"
+	name_analysis "github.com/williballenthin/Lancelot/analysis/function/name"
+	stack_delta_analysis "github.com/williballenthin/Lancelot/analysis/function/stack_delta"
 	"github.com/williballenthin/Lancelot/artifacts"
-	"github.com/williballenthin/Lancelot/disassembly"
 	peloader "github.com/williballenthin/Lancelot/loader/pe"
 	log_persistence "github.com/williballenthin/Lancelot/persistence/log"
 	mem_persistence "github.com/williballenthin/Lancelot/persistence/memory"
@@ -65,18 +63,13 @@ func doit(path string) error {
 	d, e := linear_disassembler.New(ws)
 	check(e)
 
-	var lifo []AS.VA
-
-	dis, e := disassembly.New(ws)
+	sda, e := stack_delta_analysis.New(ws)
 	check(e)
 
-	sda, e := SDA.New(ws)
+	na, e := name_analysis.New(ws)
 	check(e)
 
-	na, e := N.New(ws)
-	check(e)
-
-	dca, e := DCA.New(ws)
+	dca, e := direct_call_analysis.New(ws)
 	check(e)
 
 	hSda, e := ws.RegisterFunctionAnalysis(sda)
@@ -91,10 +84,10 @@ func doit(path string) error {
 	check(e)
 	defer ws.UnregisterFunctionAnalysis(hDca)
 
-	ep, e := EP.New(ws)
+	ep, e := entry_point_analysis.New(ws)
 	check(e)
 
-	pro, e := Pr.New(ws)
+	pro, e := prologue_analysis.New(ws)
 	check(e)
 
 	hEp, e := ws.RegisterFileAnalysis(ep)
@@ -105,84 +98,11 @@ func doit(path string) error {
 	check(e)
 	defer ws.UnregisterFileAnalysis(hPro)
 
-	// callback for drawing instructions nicely
-	d.RegisterInstructionTraceHandler(func(insn gapstone.Instruction) error {
-		s, _, e := linear_disassembler.FormatAddressDisassembly(
-			dis, ws, AS.VA(insn.Address),
-			ws.DisplayOptions.NumOpcodeBytes)
-		check(e)
-
-		if disassembly.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_CALL) {
-			if insn.X86.Operands[0].Type == gapstone.X86_OP_MEM {
-				// assume we have: call [0x4010000]  ; IAT
-				iva := AS.VA(insn.X86.Operands[0].Mem.Disp)
-				sym, e := ws.ResolveAddressToSymbol(iva)
-				if e == nil {
-					s = s + fmt.Sprintf("  ; %s.%s", sym.ModuleName, sym.SymbolName)
-				}
-			} else if insn.X86.Operands[0].Type == gapstone.X86_OP_IMM {
-				// assume we have: call 0x401000
-				targetva := AS.VA(insn.X86.Operands[0].Imm)
-				s = s + fmt.Sprintf("  ; sub_%x", targetva)
-			}
-		} else if disassembly.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_JUMP) {
-			if insn.X86.Operands[0].Type == gapstone.X86_OP_MEM {
-				// assume we have: jmp [0x4010000]  ; IAT
-				iva := AS.VA(insn.X86.Operands[0].Mem.Disp)
-				sym, e := ws.ResolveAddressToSymbol(iva)
-				if e == nil {
-					s = s + fmt.Sprintf("  ; %s.%s", sym.ModuleName, sym.SymbolName)
-				}
-			}
-		}
-
-		log.Printf(s)
-		return nil
-	})
-
-	// callback for discovering referenced functions
-	// note closure over lifo
-	d.RegisterInstructionTraceHandler(func(insn gapstone.Instruction) error {
-		if disassembly.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_CALL) {
-			if insn.X86.Operands[0].Type == gapstone.X86_OP_IMM {
-				// assume we have: call 0x401000
-				targetva := AS.VA(insn.X86.Operands[0].Imm)
-				lifo = append(lifo, targetva)
-				log.Printf("found function: sub_%s", targetva)
-			}
-			log.Printf("call: ...")
-		}
-		return nil
-	})
-
-	// callback for computing calling conventions
-	d.RegisterInstructionTraceHandler(func(insn gapstone.Instruction) error {
-		if !disassembly.DoesInstructionHaveGroup(insn, gapstone.X86_GRP_RET) {
-			return nil
-		}
-		if len(insn.X86.Operands) == 0 {
-			log.Printf("calling convention: non-stdcall")
-			return nil
-		}
-		if insn.X86.Operands[0].Type != gapstone.X86_OP_IMM {
-			return nil
-		}
-		stackDelta := insn.X86.Operands[0].Imm
-		log.Printf("calling convention: stdcall:0x%x", stackDelta)
-		return nil
-	})
-
 	// callback for recording intra-function edges
 	d.RegisterJumpTraceHandler(func(insn gapstone.Instruction, xref *artifacts.JumpCrossReference) error {
 		log.Printf("edge: %s --> %s (%s)", xref.From, xref.To, xref.Type)
 		return nil
 	})
-
-	// debugging
-	check(ws.DumpMemoryRegions())
-
-	log.Printf("============================================")
-	log.Printf("ok, thats done. now lets make a function.")
 
 	ws.AnalyzeAll()
 
