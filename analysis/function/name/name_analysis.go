f249dd366aa85c6a04e4cfc932b7fcf195dfbdae
Willi Ballenthin <willi.ballenthin@gmail.com>
2016-01-08 21:09:18 -0500

    name analysis: attempt to use exported names

--- analysis/function/name/name_analysis.go
+++ analysis/function/name/name_analysis.go
@@ -24,6 +24,11 @@ func New(ws *W.Workspace) (*NameAnalysis, error) {
 
 /** NameAnalysis implements FunctionAnalysis interface **/
 func (a *NameAnalysis) AnalyzeFunction(f *artifacts.Function) error {
-	f.SetName(fmt.Sprintf("sub_%s", f.Start))
+	sym, e := a.ws.ResolveAddressToSymbol(f.Start)
+	if e == nil {
+		f.SetName(sym.SymbolName)
+	} else {
+		f.SetName(fmt.Sprintf("sub_%s", f.Start))
+	}
 	return nil
 }
