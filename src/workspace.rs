ed5a3666f03fef7f37cadb8c328708435409802d
Willi Ballenthin <wilbal1087@gmail.com>
2019-04-25 14:30:59 -0600

    workspace: begin to implement from_bytes

--- src/workspace.rs
+++ src/workspace.rs
@@ -30,9 +30,24 @@ pub struct Workspace<A: Arch> {
 }
 
 impl<A: Arch + 'static> Workspace<A> {
-    pub fn from_bytes(filename: &str, buf: Vec<u8>) -> Result<Workspace<A>, Error> {
-        ensure!(A::get_bits() == 32, "only 32-bit loading right now");
-
+    /// Create a workspace and load the given bytes.
+    ///
+    /// Example:
+    ///
+    /// ```
+    /// use lancelot::arch::*;
+    /// use lancelot::workspace::Workspace;
+    ///
+    /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
+    ///   .map(|ws| {
+    ///     assert_eq!(ws.loader.get_name(),       "Windows/32/Raw");
+    ///     assert_eq!(ws.module.base_address,     0x0);
+    ///     assert_eq!(ws.module.sections[0].name, "raw");
+    ///   })
+    ///   .map_err(|e| panic!(e));
+    /// ```
+    pub fn from_bytes(filename: &str, buf: &[u8]) -> Result<Workspace<A>, Error> {
+        let buf = buf.to_vec();
         let (ldr, module) = loader::load::<A>(&buf)?;
 
         Ok(Workspace::<A>{
@@ -45,7 +60,7 @@ impl<A: Arch + 'static> Workspace<A> {
 
     pub fn from_file(filename: &str) -> Result<Workspace<A>, Error> {
         let buf = util::read_file(filename)?;
-        Workspace::from_bytes(filename, buf)
+        Workspace::from_bytes(filename, &buf)
     }
 
     // API:
