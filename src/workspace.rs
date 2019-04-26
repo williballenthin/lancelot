388d4166b98f838cb2e67bdfb7f1cdc6682fe268
Willi Ballenthin <wilbal1087@gmail.com>
2019-04-26 09:12:09 -0600

    workspace: use builder pattern to configure workspace load

--- src/workspace.rs
+++ src/workspace.rs
@@ -18,6 +18,82 @@ pub enum WorkspaceError {
     BufferOverrun,
 }
 
+
+pub struct WorkspaceBuilder<A: Arch> {
+    filename: String,
+    buf: Vec<u8>,
+
+    loader: Option<Box<dyn Loader<A>>>,
+}
+
+impl<A: Arch + 'static> WorkspaceBuilder<A> {
+    /// Override the default loader picker with the given loader.
+    pub fn with_loader(self: WorkspaceBuilder<A>, loader: Box<dyn Loader<A>>) -> WorkspaceBuilder<A> {
+        WorkspaceBuilder {
+            loader: Some(loader),
+            .. self
+        }
+    }
+
+    /// Construct a workspace with the given builder configuration.
+    ///
+    /// This invokes the loaders, analyzers, and another other logic,
+    ///  resulting in an initialized Workspace instance.
+    ///
+    /// Example (with default loader):
+    ///
+    /// ```
+    /// use lancelot::arch::*;
+    /// use lancelot::workspace::Workspace;
+    ///
+    /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
+    ///   .load()
+    ///   .map(|ws| {
+    ///     assert_eq!(ws.loader.get_name(),       "Windows/32/Raw");
+    ///     assert_eq!(ws.module.base_address,     0x0);
+    ///     assert_eq!(ws.module.sections[0].name, "raw");
+    ///   })
+    ///   .map_err(|e| panic!(e));
+    /// ```
+    ///
+    /// Example (with specific loader):
+    ///
+    /// ```
+    /// use lancelot::arch::*;
+    /// use lancelot::loader;
+    /// use lancelot::workspace::Workspace;
+    ///
+    /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
+    ///   .with_loader(Box::new(loader::ShellcodeLoader::<Arch32>::new(loader::Platform::Windows)))
+    ///   .load()
+    ///   .map(|ws| {
+    ///     assert_eq!(ws.loader.get_name(),       "Windows/32/Raw");
+    ///     assert_eq!(ws.module.base_address,     0x0);
+    ///     assert_eq!(ws.module.sections[0].name, "raw");
+    ///   })
+    ///   .map_err(|e| panic!(e));
+    /// ```
+    pub fn load(self: WorkspaceBuilder<A>) -> Result<Workspace<A>, Error> {
+        // if the user provided a loader, use that.
+        // otherwise, use the default detected loader.
+        let (ldr, module) = match self.loader {
+            Some(ldr) => {
+                let module = ldr.load(&self.buf)?;
+                (ldr, module)
+            }
+            None => loader::load::<A>(&self.buf)?,
+        };
+
+        Ok(Workspace::<A>{
+            filename: self.filename,
+            buf: self.buf,
+
+            loader: ldr,
+            module: module,
+        })
+    }
+}
+
 pub struct Workspace<A: Arch> {
     // name or source of the file
     pub filename: String,
@@ -38,46 +114,21 @@ pub struct Workspace<A: Arch> {
 impl<A: Arch + 'static> Workspace<A> {
     /// Create a workspace and load the given bytes.
     ///
-    /// Example:
-    ///
-    /// ```
-    /// use lancelot::arch::*;
-    /// use lancelot::workspace::Workspace;
-    ///
-    /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
-    ///   .map(|ws| {
-    ///     assert_eq!(ws.loader.get_name(),       "Windows/32/Raw");
-    ///     assert_eq!(ws.module.base_address,     0x0);
-    ///     assert_eq!(ws.module.sections[0].name, "raw");
-    ///   })
-    ///   .map_err(|e| panic!(e));
-    /// ```
-    pub fn from_bytes(filename: &str, buf: &[u8]) -> Result<Workspace<A>, Error> {
-        let buf = buf.to_vec();
-        let (ldr, module) = loader::load::<A>(&buf)?;
-
-        Ok(Workspace::<A>{
+    /// See example on `WorkspaceBuilder::load()`
+    pub fn from_bytes(filename: &str, buf: &[u8]) -> WorkspaceBuilder<A> {
+        WorkspaceBuilder {
             filename: filename.to_string(),
-            buf: buf,
-            loader: ldr,
-            module: module,
-        })
+            buf: buf.to_vec(),
+            loader: None,
+        }
     }
 
-    // TODO: maybe use the builder pattern here,
-    //  so that you can optionally specify the loader you'd like to use.
-    //
-    // For example:
-    //
-    // ```
-    //  Workspace::<Arch32>::new()
-    //    .with_file("foo.bin")
-    //    .with_loader(loader::ShellcodeLoader::<Arch32>::new(...))
-    //    .load();
-    // ```
-    pub fn from_file(filename: &str) -> Result<Workspace<A>, Error> {
-        let buf = util::read_file(filename)?;
-        Workspace::from_bytes(filename, &buf)
+    pub fn from_file(filename: &str) -> Result<WorkspaceBuilder<A>, Error> {
+        Ok(WorkspaceBuilder {
+            filename: filename.to_string(),
+            buf: util::read_file(filename)?,
+            loader: None,
+        })
     }
 
     /// Read bytes from the given RVA.
@@ -91,11 +142,12 @@ impl<A: Arch + 'static> Workspace<A> {
     ///
     /// ```
     /// use lancelot::arch::*;
+    /// use lancelot::loader;
     /// use lancelot::workspace::Workspace;
     ///
-    /// // TODO: use exactly the shellcode loader.
-    ///
     /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
+    ///   .with_loader(Box::new(loader::ShellcodeLoader::<Arch32>::new(loader::Platform::Windows)))
+    ///   .load()
     ///   .map(|ws| {
     ///     assert_eq!(ws.read_bytes(0x0, 0x1).unwrap().to_vec(), b"\xEB");
     ///     assert_eq!(ws.read_bytes(0x1, 0x1).unwrap().to_vec(), b"\xFE");
@@ -135,11 +187,12 @@ impl<A: Arch + 'static> Workspace<A> {
     ///
     /// ```
     /// use lancelot::arch::*;
+    /// use lancelot::loader;
     /// use lancelot::workspace::Workspace;
     ///
-    /// // TODO: use exactly the shellcode loader.
-    ///
     /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
+    ///   .with_loader(Box::new(loader::ShellcodeLoader::<Arch32>::new(loader::Platform::Windows)))
+    ///   .load()
     ///   .map(|ws| {
     ///     assert_eq!(ws.read_u8(0x0).unwrap(), 0xEB);
     ///     assert_eq!(ws.read_u8(0x1).unwrap(), 0xFE);
@@ -160,11 +213,12 @@ impl<A: Arch + 'static> Workspace<A> {
     ///
     /// ```
     /// use lancelot::arch::*;
+    /// use lancelot::loader;
     /// use lancelot::workspace::Workspace;
     ///
-    /// // TODO: use exactly the shellcode loader.
-    ///
     /// Workspace::<Arch32>::from_bytes("foo.bin", b"\xEB\xFE")
+    ///   .with_loader(Box::new(loader::ShellcodeLoader::<Arch32>::new(loader::Platform::Windows)))
+    ///   .load()
     ///   .map(|ws| {
     ///     assert_eq!(ws.read_u16(0x0).unwrap(), 0xFEEB);
     ///     assert_eq!(ws.read_u16(0x1).is_err(), true);
@@ -177,16 +231,17 @@ impl<A: Arch + 'static> Workspace<A> {
             .and_then(|buf| Ok(LittleEndian::read_u16(buf)))
     }
 
+    /// Read a dword from the given RVA.
     pub fn read_u32(&self, rva: A::RVA) -> Result<u32, Error> {
         self.read_bytes(rva, 4)
             .and_then(|buf| Ok(LittleEndian::read_u32(buf)))
     }
 
+    /// Read a qword from the given RVA.
     pub fn read_u64(&self, rva: A::RVA) -> Result<u64, Error> {
         self.read_bytes(rva, 8)
             .and_then(|buf| Ok(LittleEndian::read_u64(buf)))
     }
-
 
     /// Read an RVA from the given RVA.
     /// Note that the size of the read is dependent on the architecture.
@@ -197,18 +252,21 @@ impl<A: Arch + 'static> Workspace<A> {
     ///
     /// ```
     /// use lancelot::arch::*;
+    /// use lancelot::loader;
     /// use lancelot::workspace::Workspace;
     ///
-    /// // TODO: use exactly the shellcode loader.
-    ///
     /// Workspace::<Arch32>::from_bytes("foo.bin", b"\x00\x11\x22\x33")
+    ///   .with_loader(Box::new(loader::ShellcodeLoader::<Arch32>::new(loader::Platform::Windows)))
+    ///   .load()
     ///   .map(|ws| {
     ///     assert_eq!(ws.read_rva(0x0).unwrap(), 0x33221100);
+    ///     assert_eq!(ws.read_rva(0x1).is_err(), true);
     ///   })
     ///   .map_err(|e| panic!(e));
     /// ```
     pub fn read_rva(&self, rva: A::RVA) -> Result<A::RVA, Error> {
         match A::get_bits() {
+            // these conversions will never fail
             32 => Ok(A::RVA::from_u32(self.read_u32(rva)?).unwrap()),
             64 => Ok(A::RVA::from_u64(self.read_u64(rva)?).unwrap()),
             _  => panic!("unexpected architecture"),
@@ -219,6 +277,7 @@ impl<A: Arch + 'static> Workspace<A> {
     /// Note that the size of the read is dependent on the architecture.
     pub fn read_va(&self, rva: A::RVA) -> Result<A::VA, Error> {
         match A::get_bits() {
+            // these conversions will never fail
             32 => Ok(A::VA::from_u32(self.read_u32(rva)?).unwrap()),
             64 => Ok(A::VA::from_u64(self.read_u64(rva)?).unwrap()),
             _  => panic!("unexpected architecture"),
@@ -227,7 +286,6 @@ impl<A: Arch + 'static> Workspace<A> {
 
     // API:
     //   get_insn
-    //   get_byte/word/dword
     //   get_xrefs_to
     //   get_xrefs_from
     //   get_functions
