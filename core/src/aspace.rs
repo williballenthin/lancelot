b0daa625321db1202dc48906373fd67e3798a20e
Willi Ballenthin <wilbal1087@gmail.com>
2019-09-07 15:34:55 -0600

    aspace: implement slice_split

--- core/src/aspace.rs
+++ core/src/aspace.rs
@@ -1,3 +1,5 @@
+use log::{debug};
+
 use super::arch::RVA;
 
 const PAGE_SIZE: usize = 0x1000;
@@ -68,6 +70,7 @@ impl<T: Default + Copy> DenseAddressSpace<T> {
         if items.len() != PAGE_SIZE {
             panic!("invalid map buffer size");
         }
+        debug!("mapping page: {}", rva);
         if page(rva) > self.pages.len() {
             return Err(Error::NotMapped);
         }
@@ -87,7 +90,7 @@ impl<T: Default + Copy> DenseAddressSpace<T> {
     /// see example under `get`.
     pub fn map(&mut self, rva: RVA, items: &[T]) -> Result<(), Error> {
         for (i, chunk) in items.chunks_exact(PAGE_SIZE).enumerate() {
-            self.map_page(rva + i * PAGE_SIZE, chunk)?
+            self.map_page(rva + i * PAGE_SIZE, chunk)?;
         }
         Ok(())
     }
@@ -168,7 +171,7 @@ impl<T: Default + Copy> DenseAddressSpace<T> {
     /// d.map_empty(0x0.into(), 0x1000).expect("failed to map");
     /// assert_eq!(d.slice(0x0.into(), 0x2.into()).unwrap(), [0x0, 0x0]);
     /// ```
-    fn slice_simple(&self, start: RVA, end: RVA) -> Result<&[T], Error> {
+    fn slice_simple(&self, start: RVA, end: RVA) -> Result<Vec<T>, Error> {
         if page(start) > self.pages.len() {
             return Err(Error::NotMapped);
         }
@@ -180,12 +183,91 @@ impl<T: Default + Copy> DenseAddressSpace<T> {
             Some(page) => page,
         };
 
-        Ok(&page.elements[page_offset(start)..page_offset(end)])
+        Ok(page.elements[page_offset(start)..page_offset(end)].to_vec())
     }
 
-    fn slice_split(&self, start: RVA, end: RVA) -> Result<&[T], Error> {
-        // TODO
-        self.slice_simple(start, end)
+    /// handle the complex slice case: when start and end are on different pages.
+    /// for example, reading a dword from address 0xFFE.
+    ///
+    /// ```
+    /// use lancelot::arch::RVA;
+    /// use lancelot::aspace::DenseAddressSpace;
+    ///
+    /// let mut d: DenseAddressSpace<u32> = DenseAddressSpace::with_capacity(0x5000.into());
+    /// d.map_empty(0x1000.into(), 0x3000).expect("failed to map");
+    ///
+    /// // 0     unmapped
+    /// //       unmapped
+    /// // 1000  0 0 0 0
+    /// //       0 0 0 0
+    /// // 2000  0 0 0 0
+    /// //       0 0 0 0
+    /// // 3000  0 0 0 0
+    /// //       0 0 0 0
+    /// // 4000  unmapped
+    /// //       unmapped
+    /// // 5000  unmapped
+    ///
+    /// assert_eq!(d.slice(0x1FFC.into(), 0x2000.into()).unwrap(), [0x0, 0x0, 0x0, 0x0], "no overlap");
+    /// assert_eq!(d.slice(0x1FFD.into(), 0x2001.into()).unwrap(), [0x0, 0x0, 0x0, 0x0], "overlap 1");
+    /// assert_eq!(d.slice(0x1FFE.into(), 0x2002.into()).unwrap(), [0x0, 0x0, 0x0, 0x0], "overlap 2");
+    /// assert_eq!(d.slice(0x1FFF.into(), 0x2003.into()).unwrap(), [0x0, 0x0, 0x0, 0x0], "overlap 3");
+    /// assert_eq!(d.slice(0x2000.into(), 0x2004.into()).unwrap(), [0x0, 0x0, 0x0, 0x0], "overlap 4");
+    ///
+    /// assert_eq!(d.slice(0x1FFC.into(), 0x3004.into()).unwrap().len(), 0x1008, "4, page, 4");
+    ///
+    /// assert_eq!(d.slice(0x1FFC.into(), 0x3000.into()).unwrap().len(), 0x1004, "4, page");
+    ///
+    /// assert_eq!(d.slice(0x2000.into(), 0x3004.into()).unwrap().len(), 0x1004, "page, 4");
+    /// ```
+    fn slice_split(&self, start: RVA, end: RVA) -> Result<Vec<T>, Error> {
+        // precondition: end > start
+
+        if page(end) > self.pages.len() {
+            return Err(Error::NotMapped);
+        }
+
+        // ensure each page within the requested region is mapped.
+        let start_page = page(start);
+        let end_page = if page_offset(end) == 0 { page(end) - 1} else { page(end) };
+        for page in start_page..end_page {
+            if ! self.probe((page * PAGE_SIZE).into()) {
+                return Err(Error::NotMapped);
+            }
+        }
+
+        let mut ret: Vec<T> = Vec::with_capacity((end - start).into());
+
+        // region one: from `start` to the end of its page
+        // region two: any intermediate complete pages
+        // region three: from start of final page until `end`
+
+        // one.
+        {
+            let page = self.pages[page(start)].as_ref().unwrap();
+            let buf = &page.elements[page_offset(start)..];
+            ret.extend_from_slice(buf);
+        }
+
+        // two.
+        if page(start) != page(end) - 1 {
+            let start_index = page(start) + 1;
+            let end_index = page(end);
+            for page_index in start_index..end_index {
+                let page = self.pages[page_index].as_ref().unwrap();
+                let buf = &page.elements[..];
+                ret.extend_from_slice(buf);
+            }
+        }
+
+        // three.
+        if page_offset(end) != 0x0 {
+            let page = self.pages[page(end)].as_ref().unwrap();
+            let buf = &page.elements[..page_offset(end)];
+            ret.extend_from_slice(buf);
+        }
+
+        Ok(ret)
     }
 
     /// fetch the items found in the given range.
@@ -195,7 +277,7 @@ impl<T: Default + Copy> DenseAddressSpace<T> {
     ///
     /// panic if:
     ///   - start > end
-    pub fn slice(&self, start: RVA, end: RVA) -> Result<&[T], Error> {
+    pub fn slice(&self, start: RVA, end: RVA) -> Result<Vec<T>, Error> {
         if start > end {
             panic!("start > end");
         }
@@ -206,4 +288,6 @@ impl<T: Default + Copy> DenseAddressSpace<T> {
             self.slice_split(start, end)
         }
     }
+
+    // TODO: slice_into
 }
