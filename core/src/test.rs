6a3fd5902d2f7578da2db8e00fbde76fb3fce562
Willi Ballenthin <wilbal1087@gmail.com>
2019-09-16 11:08:20 -0600

    test: add logging config

--- core/src/test.rs
+++ core/src/test.rs
@@ -43,3 +43,26 @@ pub fn get_rsrc_workspace(rsrc: Rsrc) -> Workspace {
         .load()
         .unwrap()
 }
+
+
+/// configure a global logger at level==DEBUG.
+pub fn init_logging() {
+    let log_level = log::LevelFilter::Debug;
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{} [{:5}] {} {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                if log_level == log::LevelFilter::Trace {record.target()} else {""},
+                message
+            ))
+        })
+        .level(log_level)
+        .chain(std::io::stderr())
+        .filter(|metadata| {
+            !metadata.target().starts_with("goblin::pe")
+        })
+        .apply()
+        .expect("failed to configure logging");
+}
