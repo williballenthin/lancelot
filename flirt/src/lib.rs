1f9f0db8bab1cd25424c84a00c14f878cfde196e
Willi Ballenthin <wilbal1087@gmail.com>
2019-10-08 17:02:56 -0600

    flirt: remove regex-based scanner

--- flirt/src/lib.rs
+++ flirt/src/lib.rs
@@ -130,10 +130,6 @@ impl FlirtSignature {
         FlirtSignatureMatcher::new(self)
     }
 
-    pub fn create_scanner(&self) -> FlirtSignatureScanner {
-        FlirtSignatureScanner::new(self)
-    }
-
     pub fn get_name(&self) -> Option<&str> {
         for name in self.names.iter() {
             if let Symbol::Public(name) = name {
@@ -293,90 +289,5 @@ impl<'a> FlirtSignatureMatcher<'a> {
 
         trace!("flirt signature: match");
         true
-    }
-}
-
-pub struct FlirtSignatureScanner<'a> {
-    re: Regex,
-    sig: &'a FlirtSignature,
-    matcher: FlirtSignatureMatcher<'a>,
-}
-
-impl<'a> FlirtSignatureScanner<'a> {
-    pub fn new(sig: &'a FlirtSignature) -> FlirtSignatureScanner {
-        FlirtSignatureScanner {
-            re: FlirtSignatureScanner::create_scan_re(sig),
-            sig: sig,
-            matcher: sig.create_matcher(),
-        }
-    }
-
-    /// translate the given FLIRT byte signature into a binary regex.
-    ///
-    /// it captures matches from anywhere in the input,
-    /// so its best for the `find_iter` operation.
-    fn create_scan_re(sig: &'a FlirtSignature) -> Regex {
-        // the `(?-u)` disables unicode mode, which lets us match raw byte values.
-        let pattern = format!("(?-u){}", create_pattern(sig));
-
-        Regex::new(&pattern).expect("failed to compile regex")
-    }
-
-    /// scan the given buffer for FLIRT signature matches.
-    ///
-    /// the complexity is:
-    ///   - regex scan for pattern across entire buffer
-    ///   - (duplicate) regex scan of match
-    ///   - crc16 of N subsequent bytes (usually around 0x20)
-    ///
-    /// ```
-    /// use flirt;
-    /// use flirt::pat;
-    ///
-    /// let pat_buf = "\
-    /// 518B4C240C895C240C8D5C240C508D442408F7D923C18D60F88B43F08904248B 21 B4FE 006E :0000 __EH_prolog3_GS_align ^0041 ___security_cookie ........33C5508941FC8B4DF0895DF08B4304894504FF75F464A1000000008945F48D45F464A300000000F2C3
-    /// 518B4C240C895C240C8D5C240C508D442408F7D923C18D60F88B43F08904248B 1F E4CF 0063 :0000 __EH_prolog3_align ^003F ___security_cookie ........33C5508B4304894504FF75F464A1000000008945F48D45F464A300000000F2C3
-    /// 518B4C240C895C240C8D5C240C508D442408F7D923C18D60F88B43F08904248B 22 E4CE 006F :0000 __EH_prolog3_catch_GS_align ^0042 ___security_cookie ........33C5508941FC8B4DF08965F08B4304894504FF75F464A1000000008945F48D45F464A300000000F2C3
-    /// 518B4C240C895C240C8D5C240C508D442408F7D923C18D60F88B43F08904248B 20 6562 0067 :0000 __EH_prolog3_catch_align ^0040 ___security_cookie ........33C5508965F08B4304894504FF75F464A1000000008945F48D45F464A300000000F2C3
-    /// ---";
-    ///
-    /// let sigs = pat::parse(pat_buf).unwrap();
-    /// let __EH_prolog3_catch_align = &sigs[3];
-    /// let m = __EH_prolog3_catch_align.create_scanner();
-    /// assert_eq!(m.scan(&[
-    ///     // some junk
-    ///     0xFF, 0xFF, 0xFF, 0xFF,
-    ///
-    ///     // apds.dll / 4FD932C41DF96D019DC265E26E94B81B
-    ///     // __EH_prolog3_catch_align
-    ///
-    ///     // first 0x20
-    ///     0x51, 0x8B, 0x4C, 0x24, 0x0C, 0x89, 0x5C, 0x24,
-    ///     0x0C, 0x8D, 0x5C, 0x24, 0x0C, 0x50, 0x8D, 0x44,
-    ///     0x24, 0x08, 0xF7, 0xD9, 0x23, 0xC1, 0x8D, 0x60,
-    ///     0xF8, 0x8B, 0x43, 0xF0, 0x89, 0x04, 0x24, 0x8B,
-    ///     // crc16 start
-    ///     0x43, 0xF8, 0x50, 0x8B, 0x43, 0xFC, 0x8B, 0x4B,
-    ///     0xF4, 0x89, 0x6C, 0x24, 0x0C, 0x8D, 0x6C, 0x24,
-    ///     0x0C, 0xC7, 0x44, 0x24, 0x08, 0xFF, 0xFF, 0xFF,
-    ///     0xFF, 0x51, 0x53, 0x2B, 0xE0, 0x56, 0x57, 0xA1,
-    ///     // crc end
-    ///     0xD4, 0xAD, 0x19, 0x01, 0x33, 0xC5, 0x50, 0x89,
-    ///     0x65, 0xF0, 0x8B, 0x43, 0x04, 0x89, 0x45, 0x04,
-    ///     0xFF, 0x75, 0xF4, 0x64, 0xA1, 0x00, 0x00, 0x00,
-    ///     0x00, 0x89, 0x45, 0xF4, 0x8D, 0x45, 0xF4, 0x64,
-    ///     0xA3, 0x00, 0x00, 0x00, 0x00, 0xC3,
-    ///     // end function
-    ///
-    ///     // some junk
-    ///     0xFF, 0xFF, 0xFF, 0xFF,
-    /// ]), [0x4]);
-    /// ```
-    pub fn scan(&self, buf: &[u8]) -> Vec<usize> {
-        self.re
-            .find_iter(buf)
-            .map(|m| m.start())
-            .filter(|&start| self.matcher.r#match(&buf[start..]))
-            .collect()
     }
 }
