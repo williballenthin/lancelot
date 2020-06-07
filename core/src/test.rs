27484222f730d22f6f2406fbe2b95dca887cc7a2
William Ballenthin <william.ballenthin@fireeye.com>
2020-06-07 05:39:04 -0600

    test: add helpers to construct module from shellcode

--- core/src/test.rs
+++ core/src/test.rs
@@ -1,5 +1,11 @@
 //< Helpers that are useful for tests and doctests.
 
+use anyhow::Result;
+
+use crate::aspace::RelativeAddressSpace;
+use crate::module::{Arch, Module, Permissions, Section};
+use crate::RVA;
+
 /// configure a global logger at level==DEBUG.
 pub fn init_logging() {
     let log_level = log::LevelFilter::Debug;
@@ -22,4 +28,34 @@ pub fn init_logging() {
         .filter(|metadata| !metadata.target().starts_with("goblin::pe"))
         .apply()
         .expect("failed to configure logging");
+}
+
+pub fn load_shellcode(arch: Arch, buf: &[u8]) -> Result<Module> {
+    let mut address_space = RelativeAddressSpace::with_capacity(buf.len() as u64);
+    address_space.map.writezx(0x0, buf)?;
+
+    Ok(Module {
+        arch,
+        sections: vec![Section {
+            name: "shellcode".to_string(),
+            perms: Permissions::RWX,
+            physical_range: std::ops::Range {
+                start: 0x0,
+                end: buf.len() as RVA,
+            },
+            virtual_range: std::ops::Range {
+                start: 0x0,
+                end: buf.len() as RVA,
+            },
+        }],
+        address_space: address_space.into_absolute(0x0)?,
+    })
+}
+
+pub fn load_shellcode32(buf: &[u8]) -> Result<Module> {
+    load_shellcode(Arch::X32, buf)
+}
+
+pub fn load_shellcode64(buf: &[u8]) -> Result<Module> {
+    load_shellcode(Arch::X32, buf)
 }
