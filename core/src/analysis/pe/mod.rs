56ccc36664352f3ea7b071c83d943129621cf478
William Ballenthin <william.ballenthin@fireeye.com>
2020-06-08 19:03:42 -0600

    analysis: find function starts

--- core/src/analysis/pe/mod.rs
+++ core/src/analysis/pe/mod.rs
@@ -1,3 +1,10 @@
+use std::collections::HashSet;
+
+use anyhow::Result;
+
+use crate::loader::pe::PE;
+use crate::VA;
+
 pub mod call_targets;
 pub mod control_flow_guard;
 pub mod entrypoints;
@@ -5,3 +12,27 @@ pub mod exports;
 pub mod pointers;
 pub mod safeseh;
 //pub mod patterns;
+
+pub fn find_function_starts(pe: &PE) -> Result<Vec<VA>> {
+    let mut function_starts: HashSet<VA> = Default::default();
+
+    // pointer analysis creates a good number of FPs, due to:
+    //  - exception handle
+    //  - jump table entries
+    //
+    // so, we'll disable this for now.
+    //function_starts.extend(crate::analysis::pe::pointers::find_pe_nonrelocated_executable_pointers(buf, &pe)?);
+
+    function_starts.extend(crate::analysis::pe::entrypoints::find_pe_entrypoint(&pe)?);
+    function_starts.extend(crate::analysis::pe::exports::find_pe_exports(&pe)?);
+    function_starts.extend(crate::analysis::pe::safeseh::find_pe_safeseh_handlers(&pe)?);
+    function_starts.extend(crate::analysis::pe::control_flow_guard::find_pe_cfguard_functions(&pe)?);
+    function_starts.extend(crate::analysis::pe::call_targets::find_pe_call_targets(&pe)?);
+
+    // TODO: validate that the code looks ok
+
+    let mut function_starts: Vec<_> = function_starts.into_iter().collect();
+    function_starts.sort();
+
+    Ok(function_starts)
+}
