2a8bc174398da6e23f85f71694b29c7ff6ea374f
William Ballenthin <william.ballenthin@fireeye.com>
2020-08-13 18:43:05 -0600

    ci: attempt to maturin

--- .github/workflows/build-pylancelot.sh
+++ .github/workflows/build-pylancelot.sh
@@ -4,6 +4,7 @@ set -e;
 set -x;
 
 curl https://sh.rustup.rs | sh -s -- -y;
+export PATH="$PATH:~/.cargo/bin/";
 rustup set profile minimal;
 rustup toolchain install nightly;
 rustup override set nightly;
