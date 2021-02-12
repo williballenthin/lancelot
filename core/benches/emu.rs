78332d5b404598456f20a897f82d7fe6698fb33f
William Ballenthin <william.ballenthin@fireeye.com>
2021-02-12 12:28:50 -0700

    emu: bench: push

--- core/benches/emu.rs
+++ core/benches/emu.rs
@@ -13,12 +13,21 @@ fn fetch_benchmark(c: &mut Criterion) {
 }
 
 fn insn_benchmark(c: &mut Criterion) {
-    // 0:  48 c7 c0 01 00 00 00    mov    rax,0x1
-    let mut emu = lancelot::test::emu_from_shellcode64(&b"\x48\xC7\xC0\x01\x00\x00\x00"[..]);
-
     c.bench_function("mov rax, 0x1", |b| {
+        // 0:  48 c7 c0 01 00 00 00    mov    rax,0x1
+        let mut emu = lancelot::test::emu_from_shellcode64(&b"\x48\xC7\xC0\x01\x00\x00\x00"[..]);
         b.iter(|| {
             emu.reg.rip = 0x0;
+            emu.step().unwrap();
+        })
+    });
+
+    c.bench_function("push 0x1", |b| {
+        // 0:  6a 01                   push   0x1
+        let mut emu = lancelot::test::emu_from_shellcode64(&b"\x6A\x01"[..]);
+        b.iter(|| {
+            emu.reg.rip = 0x0;
+            emu.reg.rsp = 0x6000;
             emu.step().unwrap();
         })
     });
