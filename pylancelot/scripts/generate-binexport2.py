b984df34b2355be6bc0bc05615ebed1544abded8
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-09-27 10:46:37 +0000

    pylancelot: add script to export binexport2

---
+++ pylancelot/scripts/generate-binexport2.py
@@ -0,0 +1,14 @@
+import sys
+
+import lancelot
+
+
+with open(sys.argv[1], "rb") as f:
+    buf = f.read()
+
+
+be2 = lancelot.binexport2_from_bytes(buf)
+with open(sys.argv[2], "wb") as f:
+    f.write(be2)
+
+print(f"wrote {len(be2)} bytes!")
