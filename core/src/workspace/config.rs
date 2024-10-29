f1951ab82ddcc81d10c48dd18f1a9922c6473cfe
Willi Ballenthin <wballenthin@google.com>
2024-10-29 14:32:50 +0000

    config: add DynamicConfiguration

--- core/src/workspace/config.rs
+++ core/src/workspace/config.rs
@@ -80,3 +80,57 @@ impl Configuration for FileSystemConfiguration {
         })
     }
 }
+
+#[derive(Default)]
+pub struct DynamicConfiguration {
+    sig_paths:      Vec<PathBuf>,
+    function_hints: Vec<VA>,
+}
+
+impl DynamicConfiguration {
+    pub fn with_sig_path(mut self, sig_path: &Path) -> DynamicConfiguration {
+        self.sig_paths.push(sig_path.to_path_buf());
+        self
+    }
+
+    pub fn with_sig_paths(mut self, sig_paths: &[PathBuf]) -> DynamicConfiguration {
+        self.sig_paths.extend_from_slice(sig_paths);
+        self
+    }
+
+    pub fn with_function_hints(mut self, function_hints: &[VA]) -> DynamicConfiguration {
+        self.function_hints.extend_from_slice(function_hints);
+        self
+    }
+}
+
+impl Configuration for DynamicConfiguration {
+    fn get_sigs(&self) -> Result<FlirtSignatureSet> {
+        let mut sigs: Vec<FlirtSignature> = Default::default();
+
+        for sig_path in self.sig_paths.iter() {
+            if let Some(filename) = sig_path.file_name() {
+                if filename.to_string_lossy().ends_with(".sig") {
+                    let buf = std::fs::read(sig_path.as_path())?;
+                    sigs.extend(lancelot_flirt::sig::parse(&buf)?);
+                } else if filename.to_string_lossy().ends_with(".pat") {
+                    let buf = String::from_utf8(std::fs::read(sig_path.as_path())?)?;
+                    sigs.extend(lancelot_flirt::pat::parse(&buf)?);
+                }
+            }
+        }
+
+        Ok(FlirtSignatureSet::with_signatures(sigs))
+    }
+
+    fn get_function_hints(&self) -> Result<Vec<VA>> {
+        Ok(self.function_hints.clone())
+    }
+
+    fn clone(&self) -> Box<dyn Configuration> {
+        Box::new(DynamicConfiguration {
+            sig_paths:      self.sig_paths.clone(),
+            function_hints: self.function_hints.clone(),
+        })
+    }
+}
