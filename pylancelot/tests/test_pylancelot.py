be6826ade5eac0e7c024335cfb17c0d3ca977f3a
Willi Ballenthin <wballenthin@google.com>
2024-10-29 13:46:05 +0000

    simplify pylancelot API
    remove all the custom API methods/constants/accessors/etc. in deference to BinExport2
    
    closes #204

--- pylancelot/tests/test_pylancelot.py
+++ pylancelot/tests/test_pylancelot.py
@@ -4,152 +4,27 @@ import pytest
 import lancelot
 
 
-def test_invalid_pe():
-    with pytest.raises(ValueError):
-        lancelot.from_bytes(b"")
-
-    with pytest.raises(ValueError):
-        lancelot.from_bytes(b"MZ\x9000")
-
-    with contextlib.suppress(ValueError):
-        lancelot.from_bytes(b"")
-
-
-def test_load_pe(k32):
-    lancelot.from_bytes(k32)
-
-
-def test_load_coff(altsvc):
-    lancelot.from_bytes(altsvc)
-
-
-def test_arch(k32):
-    ws = lancelot.from_bytes(k32)
-    assert "Returns: str" in lancelot.Workspace.arch.__doc__
-    assert ws.arch == "x64"
-
-
-def test_base_address(k32):
-    ws = lancelot.from_bytes(k32)
-    assert "Returns: int" in lancelot.Workspace.base_address.__doc__
-    assert ws.base_address == 0x180000000
-
-
-def test_functions(k32):
-    ws = lancelot.from_bytes(k32)
-
-    assert "Returns: List[int]" in ws.get_functions.__doc__
-    functions = ws.get_functions()
-
-    # IDA identifies 2326
-    # lancelot identifies around 2200
-    assert len(functions) > 1500
-
-    # this is _security_check_cookie
-    assert 0x180020250 in functions
-
-
-def test_flow_const():
-    assert lancelot.FLOW_TYPE_FALLTHROUGH == 0
-    assert lancelot.FLOW_TYPE_CALL == 1
-
-
-def test_cfg(k32):
-    ws = lancelot.from_bytes(k32)
-
-    assert "Returns: CFG" in ws.build_cfg.__doc__
-    # this is _report_gsfailure
-    # it has a diamond shape
-    cfg = ws.build_cfg(0x1800202B0)
-
-    assert cfg.address == 0x1800202B0
-    assert 0x1800202B0 in cfg.basic_blocks
-    assert 0x180020334 in cfg.basic_blocks
-    assert 0x1800202F3 in cfg.basic_blocks
-    assert 0x180020356 in cfg.basic_blocks
-
-    bb0 = cfg.basic_blocks[0x1800202B0]
-    assert 0x180020334 in map(lambda flow: flow[lancelot.FLOW_VA], bb0.successors)
-    assert 0x1800202F3 in map(lambda flow: flow[lancelot.FLOW_VA], bb0.successors)
-
-
-def test_read_insn(k32):
-    ws = lancelot.from_bytes(k32)
-
-    assert "Returns: Instruction" in ws.read_insn.__doc__
-
-    with pytest.raises(ValueError):
-        ws.read_insn(0x0)
-
-    # .text:00000001800202B0 48 89 4C 24 08  mov     [rsp+arg_0], rcx
-    insn = ws.read_insn(0x1800202B0)
-    assert insn.address == 0x1800202B0
-    assert insn.length == 5
-    assert insn.mnemonic == "mov"
-
-    operands = insn.operands
-    assert len(operands) == 2
-
-    # op[0] == [rsp + 8]
-    assert operands[0][lancelot.OPERAND_TYPE] == lancelot.OPERAND_TYPE_MEMORY
-    assert operands[0][lancelot.OPERAND_SIZE] == 64
-    assert operands[0][lancelot.MEMORY_OPERAND_BASE] == "rsp"
-    assert operands[0][lancelot.MEMORY_OPERAND_DISP] == 8
-
-    # op[1] == rcx
-    assert operands[1][lancelot.OPERAND_TYPE] == lancelot.OPERAND_TYPE_REGISTER
-    assert operands[1][lancelot.OPERAND_SIZE] == 64
-    assert operands[1][lancelot.REGISTER_OPERAND_REGISTER] == "rcx"
-
-    assert operands == ((1, 64, "rsp", None, "ss", 0, 8), (3, 64, "rcx"))
-
-
-def test_read_bytes(k32):
-    ws = lancelot.from_bytes(k32)
-
-    assert "Returns: bytes" in ws.read_bytes.__doc__
-
-    with pytest.raises(ValueError):
-        ws.read_bytes(0x0, 1)
-
-    assert ws.read_bytes(0x180000000, 2) == b"MZ"
-
-
-def test_read_pointer(k32):
-    ws = lancelot.from_bytes(k32)
-
-    assert "Returns: int" in ws.read_pointer.__doc__
-
-    with pytest.raises(ValueError):
-        ws.read_pointer(0x0)
-
-    assert ws.read_pointer(0x180076008) == 0x18007D630
-
-
-def test_probe(k32):
-    ws = lancelot.from_bytes(k32)
-
-    assert ws.probe(0x180000000) & lancelot.PERMISSION_READ != 0
-    assert ws.probe(0x180000000) & lancelot.PERMISSION_WRITE == 0
-    assert ws.probe(0x180000000) & lancelot.PERMISSION_EXECUTE == 0
-
-    # part of some functino
-    assert ws.probe(0x1800202B0) & lancelot.PERMISSION_READ != 0
-    assert ws.probe(0x1800202B0) & lancelot.PERMISSION_WRITE == 0
-    assert ws.probe(0x1800202B0) & lancelot.PERMISSION_EXECUTE != 0
-
-
-def test_insn_int(k32):
-    ws = lancelot.from_bytes(k32)
-    assert int(ws.read_insn(0x1800202B0)) == 0x1800202B0
-
-
 def test_binexport2(k32):
     buf = lancelot.binexport2_from_bytes(k32)
     assert buf is not None
     assert isinstance(buf, bytes)
 
-    ws = lancelot.from_bytes(k32)
-    buf = ws.to_binexport2()
-    assert buf is not None
-    assert isinstance(buf, bytes)
+
+def test_invalid_pe():
+    with pytest.raises(ValueError):
+        lancelot.binexport2_from_bytes(b"")
+
+    with pytest.raises(ValueError):
+        lancelot.binexport2_from_bytes(b"MZ\x9000")
+
+    with contextlib.suppress(ValueError):
+        lancelot.binexport2_from_bytes(b"")
+
+
+def test_load_pe(k32):
+    lancelot.binexport2_from_bytes(k32)
+
+
+def test_load_coff(altsvc):
+    lancelot.binexport2_from_bytes(altsvc)
+
